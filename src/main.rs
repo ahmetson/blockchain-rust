ec485fc7ddbe08c5e83e51722ae371889c82b427
Medet Ahmetson <admin@blocklords.io>
2020-12-01 22:22:44 +0700

    mining block

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use blockchainlib::*;
 
 fn main () {
-    let mut block = Block::new(0, 0, vec![0; 32], 0, "Genesis block".to_owned());
+    let mut block = Block::new(0, 0, vec![0; 32], 0, "Genesis block".to_owned(),
+    0x00ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
 
     println!("{:?}", &block);
 
     let h = block.hash();
 
     println!("{:?}", &h);
-    
-    block.hash = h;
 
+    block.mine();
+    
     println!("{:?}", &block);    
 }
